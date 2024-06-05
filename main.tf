23167971e0be59f51df5ace4ad1d3cd3b31e1e6d
nomoredrunker61 <nomoredrunker61@gmail.com>
2024-06-05 14:45:06 +0900

    2nd gen

---
+++ main.tf
@@ -0,0 +1,29 @@
+﻿terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.41.0"
+    }
+  }
+  cloud {
+    organization = "ueno-eibundo"
+  workspaces {
+      name = "aws-infra"
+    }
+  }
+}
+
+provider "aws" {
+  region  = "ap-northeast-1"
+}
+
+resource "aws_instance" "test_server" {
+  ami           = "ami-0eba6c58b7918d3a1"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "TestInstance"
+  }
+}
+
+
